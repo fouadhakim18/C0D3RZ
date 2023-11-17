0221e9ef39def181055ca9b531367a6bfd487601
Hakim Maroc <fouadmaroc625@gmail.com>
2023-11-17 14:02:11 +0100

    sign up and log in

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:coders/views/auth_screen/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'consts/colors.dart';
 import 'consts/styles.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
